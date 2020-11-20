407d53aca0f3a386bf7c80a797e6fb1251dc86c2
nghiant11 <nghiant11@vng.com.vn>
2020-11-20 11:16:54 +0700

    upadate

--- alg/array/numbersofsmallernumber/main.go
+++ alg/array/numbersofsmallernumber/main.go
@@ -1,39 +1,23 @@
 package numbersofsmallernumber
 
-import (
-	"sort"
+const (
+	lowerBound = 0
+	upperBound = 100
 )
 
 func numberOfSmallerNumbers(numbers []int) []int {
-	m := buildFrequencyMap(numbers)
-	s := buildSliceSortByKey(m)
-	accumulativeCount := 0
-	for _, number := range s {
-		count := m[number]
-		m[number] = accumulativeCount
-		accumulativeCount += count
+	countings := make([]int, upperBound-lowerBound+1)
+	for _, number := range numbers {
+		countings[number]++
+	}
+	accumulative := 0
+	for i := range countings {
+		tmp := countings[i]
+		countings[i] = 	accumulative
+		accumulative += tmp
 	}
 	for i, n := range numbers {
-		numbers[i] = m[n]
+		numbers[i] = countings[n]
 	}
 	return numbers
-}
-
-func buildFrequencyMap(numbers []int) map[int]int {
-	m := make(map[int]int, len(numbers))
-	for _, n := range numbers {
-		m[n]++
-	}
-	return m
-}
-
-func buildSliceSortByKey(m map[int]int) []int {
-	s := make([]int, len(m))
-	i := 0
-	for k := range m {
-		s[i] = k
-		i++
-	}
-	sort.Ints(s)
-	return s
 }
